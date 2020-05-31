2adceae09d45741676bfbb8f98ee3da5aba6f758
Gerard Banasig <tuxrace@yahoo.com>
2020-06-01 00:12:43 +0800

    Updates

---
+++ precache-manifest.7614c96dd0998a3fe24087548aefa616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6544dd10355ddec6a8be20d67cb5653c",
+    "url": "/react-ad-login/index.html"
+  },
+  {
+    "revision": "2ec0cd7b18a077bb5e98",
+    "url": "/react-ad-login/static/css/main.8c366046.chunk.css"
+  },
+  {
+    "revision": "1a721b7e971d4cb77341",
+    "url": "/react-ad-login/static/js/2.7943d0b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ad-login/static/js/2.7943d0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec0cd7b18a077bb5e98",
+    "url": "/react-ad-login/static/js/main.11655602.chunk.js"
+  },
+  {
+    "revision": "28a57af10659e3399850",
+    "url": "/react-ad-login/static/js/runtime-main.f2dde063.js"
+  }
+]);
