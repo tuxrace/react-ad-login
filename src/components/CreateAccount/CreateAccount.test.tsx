be6bc46b71c06b6f18a9c76fee13751ca2f340ea
Gerard Banasig <tuxrace@yahoo.com>
2020-06-07 13:14:55 +0800

    Add Create Account test case

--- src/components/CreateAccount/CreateAccount.test.tsx
+++ src/components/CreateAccount/CreateAccount.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import CreateAccount from './CreateAccount';
 
 test('Renders CreateAccount', async () => {
     const props = {
         setCreate: jest.fn(),
     };
-    const { getByText } = render(<CreateAccount {...props} />);
+    const { getByText, container } = render(<CreateAccount {...props} />);
     expect(getByText(/Sign In/i)).toBeInTheDocument();
+    fireEvent.change(container.querySelector("input[id='firstName']") as HTMLInputElement, {target: {value: ''}})
+    fireEvent.click(container.querySelector("button") as HTMLButtonElement);
 });
